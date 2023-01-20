bc684b290e1a5c149c2131fe0944de5d89357929
Shivam Chourasia <shivamchourasia15@gmail.com>
2023-01-21 00:19:13 +0530

    added vercel config

--- server.js
+++ server.js
@@ -33,3 +33,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`));
+
+module.exports = app;
